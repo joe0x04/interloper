d08f9b73a7c5adaf400c19dce82ed5ec7bd598df
Joe Reid <joe@joereid.com>
2022-02-17 14:23:52 -0500

    Testing community stuff

--- handlers.go
+++ handlers.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"net/http"
 )
@@ -11,4 +12,30 @@ import (
  */
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Index here")
+
+	/**** testing
+	c := Community{
+		uuid:           CreateUUID(),
+		fullname:       "The Construct",
+		shortname:      "const",
+		date_created:   DBNow(),
+		date_lastvisit: 0,
+		post_count:     0,
+		feature_mask:   0,
+		creator:        0,
+	}
+
+	CommunityCreate(c)
+	*/
+	c := Community{}
+	err := CommunityGet(2, &c)
+	if err != nil {
+		if err != sql.ErrNoRows {
+			fmt.Fprintln(w, err)
+			return
+		}
+	}
+
+	//fmt.Fprintf(w, "%s - %s", c.fullname, c.uuid)
+	fmt.Fprintln(w, c)
 }
